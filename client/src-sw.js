f92fa7a85fed6737200be19b9bc83b028bc36b0a
ZETA <ben.r.dominguez@gmail.com>
2022-03-12 16:44:24 -0800

    pushing to heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,10 +1,10 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { StaleWhileRevalidate } = require('workbox-strategies');
+const { offlineFallBack, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,16 +27,13 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+/* offlineFallBack() */
 // TODO: Implement asset caching
-// offlineFallback();
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    // Name of the cache storage.
     cacheName: 'asset-cache',
     plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
